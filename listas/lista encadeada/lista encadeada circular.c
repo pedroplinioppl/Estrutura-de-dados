cebb6983b8cc98949d7b73624bc1f419990943c2
Pedro Henrique <139030477+pedroplinioppl@users.noreply.github.com>
2026-03-20 19:53:48 +0000

    Lista encadeada circular

--- listas/lista encadeada/lista encadeada circular.c
+++ listas/lista encadeada/lista encadeada circular.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+A ideia da lista encadeada circular é a célula final apontar pro início.
+*/
+
 typedef struct sCel {
     int num;
     struct sCel *prox; 
