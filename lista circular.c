99730223a38fb8a5e0d5b261b2c36ad86be80d26
Pedro Henrique <139030477+pedroplinioppl@users.noreply.github.com>
2026-03-21 19:02:27 +0000

    lista estatica circular

--- lista circular.c
+++ lista circular.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-#define TAMMAX 5
+/*
+A ideia da lista circular estática é sempre que aumentar ou diminuir, considerar usar módulo pra que o índice sempre esteja no
+intervalo da lista, fora isso, não da pra usar uma convenção normal de tamanho pois o início nem sempre é zero
+ e nem sempre fim > inicio
+*/
+
+#define MAX 5
 
 typedef struct f {
-    int arr[TAMMAX];
+    int arr[MAX];
     int inicio;
     int fim;
 } lista;
 
 void inicializar(lista *l) {
-    l->inicio = TAMMAX - 1;
-    l->fim = TAMMAX - 1;
+    l->inicio = MAX - 1;
+    l->fim = MAX - 1;
 }
 
 int estaVazia(lista *l) {
@@ -18,36 +24,55 @@ int estaVazia(lista *l) {
 }
 
 int estaCheia(lista *l) { 
-    return (l->fim + 1) % TAMMAX == l->inicio;
+    return (l->fim + 1) % MAX == l->inicio;
+}
+
+int tamanho(lista *l) {
+    return (l->fim - l->inicio + MAX) % MAX; 
 }
 
 int inserirFim(lista *l, int x) {
     if(estaCheia(l)) 
     {
-        printf("/nERRO: lista cheia. \n");
         return 1;
     }
-    l->fim = ((*l).fim + 1) % TAMMAX;
+    l->fim = (l->fim + 1) % MAX;
     l->arr[l->fim] = x;
 
     return 0;
 }
 
-int inserirInicio(lista *l, int x) {
+int inserirInicio(lista *l, int elem) {
     if(estaCheia(l)) 
     {
-        printf("Nao da nao pae");
         return 1;
     }
 
     int i = l->fim;
-    while(i != (l->inicio - 1 + TAMMAX) % TAMMAX) {
-        l->arr[(i + 1) % TAMMAX] = l->arr[i];
-        i = (i - 1 + TAMMAX) % TAMMAX;
+    while(i != (l->inicio - 1 + MAX) % MAX) {
+        l->arr[(i + 1) % MAX] = l->arr[i];
+        i = (i - 1 + MAX) % MAX;
     }
 
-    l->arr[l->inicio] = x;
-    l->fim = (l->fim + 1) % TAMMAX;
+    l->inicio = (l->inicio - 1 + MAX) % MAX;
+    l->arr[(l->inicio + 1) % MAX] = elem;
+    l->fim = (l->fim + 1) % MAX;
+    return 0;
+}
+
+int inserirPos(lista *l, int pos, int elem) {
+    if(estaCheia(l) || pos < 0 || pos > tamanho(l)) {
+        return 1;
+    }
+
+    pos = (l->inicio + 1 + pos) % MAX; // Pos tem que ser ajustado pois o inicio nem sempre é em 0
+    int i = l->fim;
+    while(i != (pos - 1 + MAX) % MAX) {
+        l->arr[(i + 1) % MAX] = l->arr[i];
+        i = (i - 1 + MAX) % MAX;
+    }
+    l->arr[pos] = elem;
+    l->fim = (l->fim + 1) % MAX;
     return 0;
 }
 
@@ -56,21 +81,51 @@ int removerInicio(lista *l) {
         return 1;
     }
 
-    l->inicio = (l->inicio + 1) % TAMMAX; 
+    l->inicio = (l->inicio + 1) % MAX; 
 
     return 0;
 }
 
-void imprimir(lista *l) {
-    int i = (l->inicio + 1) % TAMMAX;
-    if(!vazia(l)) {
-        printf("\nFila: ");
-        while(i != ((l->fim + 1) % TAMMAX)) {
-            printf("%d ", l->arr[i]);
-            i = (i + 1) % TAMMAX;
-        }
-    } else {
-        printf("\nFila vazia");
+int removerFim(lista *l) {
+    if(estaVazia(l)) {
+        return 1;
+    }
+
+    l->fim = (l->fim - 1 + MAX) % MAX;
+
+    return 0;
+} 
+
+int removerPos(lista *l, int pos) {
+    if(estaVazia(l) || pos < 0 || pos >= tamanho(l)) {
+        return 1;
+    }
+
+    pos = (l->inicio + 1 + pos) % MAX;
+
+    int i = pos;
+    while(i != l->fim) {
+        int prox = (i + 1) % MAX;
+        l->arr[i] = l->arr[prox];
+        i = prox;
+    }
+
+    l->fim = (l->fim - 1 + MAX) % MAX;
+
+    return 0;
+}
+
+void mostrarLista(lista *l) {
+    if(estaVazia(l)) {
+        return;
+    }
+
+    int i = (l->inicio + 1) % MAX;
+    int f = tamanho(l); // f é abreviação pra fim nesse caso
+
+    for(int c=0; c<f; c++) {
+        printf("%d ", l->arr[i]);
+        i = (i + 1) % MAX;
     }
     printf("\n");
 }
@@ -80,6 +135,18 @@ int main()
     lista l;
 
     inicializar(&l);
+    
+    inserirInicio(&l, 10);
+    inserirFim(&l, 5);
+    inserirPos(&l, 1, 12);
+    inserirFim(&l, 56);
+    
+    removerInicio(&l);
+    removerFim(&l);
+    removerPos(&l, 1);
+    
+    // A saída dá 12 
+
+    mostrarLista(&l);
 
 }
-    
